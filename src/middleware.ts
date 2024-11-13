9579ba10767893690bb8a4f10a07151c67aa7da5
Jatin Jangra <jatin9728722861@gmail.com>
2024-11-13 13:36:58 +0530

    updated middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,7 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 const isPublicRoute = createRouteMatcher([
+  "/api/cron/check-expirations",
   "/api/cron",
   "/sign-in(.*)",
   "/sign-up(.*)",
