9154fc972ac77a0266322b717bd11a2e1cebc3ca
Jatin Jangra <jatin9728722861@gmail.com>
2024-11-13 16:51:08 +0530

    fixed type error

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -6,7 +6,6 @@ import { DayPicker } from "react-day-picker"
 
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
-import { before } from "node:test"
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>
 
